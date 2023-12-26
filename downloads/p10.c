5ba24416794f08e1d33bb0183bf782439c1ddd54
41223214 <144990604+41223214@users.noreply.github.com>
2023-12-26 16:03:38 +0000

    新增練習十

---
+++ downloads/p10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x = 25, y = 15, result; // 使用固定數值 25 和 15
+
+    result = x * y; // 計算乘積
+
+    // 印出乘積
+    printf("兩個整數 %d 和 %d 的乘積 = %d\n", x, y, result);
+
+    return 0; // 表示程式執行成功
+}
