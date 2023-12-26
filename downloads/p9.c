e512abfc8cea062873659b4ac4e87c5b17b5dcc9
41223214 <144990604+41223214@users.noreply.github.com>
2023-12-26 16:01:32 +0000

    新增練習九

---
+++ downloads/p9.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() 
+{
+    int x = 25, y = 38, sum; // 使用固定數值 25 和 38
+
+    sum = x + y; // 計算總和
+
+    // 印出總和
+    printf("兩個整數 %d 和 %d 的總和 = %d\n", x, y, sum);
+
+    return 0; // 表示程式執行成功
+}
