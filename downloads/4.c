3e6dbf9bdcbc8e1ffaea356a4060c251aaecbef4
41223214 <144990604+41223214@users.noreply.github.com>
2024-01-11 06:07:07 +0000

    修改

--- downloads/4.c
+++ downloads/4.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-void main()
-{
-float a = 0.5;
-double b = 1.2;
-int c = 3;
-b = b + a + c;
-/* 輸出 a, b, c 到螢幕 */
-printf( " a = %3.1f, b = %3.1f, c = %d ", a ,b, c );
+
+int main() {
+    float a = 0.5;
+    double b = 1.2;
+    int c = 3;
+    b = b + a + c;
+
+    // 輸出 a, b, c 到螢幕
+    printf("a = %3.1f, b = %3.1f, c = %d", a, b, c);
+
+    // 返回 0 表示程序正確結束
+    return 0;
 }
